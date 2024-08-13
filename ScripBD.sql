da5de63913a74e38de8fcfbe857077c1f52457d4
DianyLiz <dlenamorado.uth.hn>
2024-08-12 22:31:05 -0600

    Examen III Parcial Diany Enamorado

---
+++ ScripBD.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE productos_db;
+USE productos_db;
+
+CREATE TABLE productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    cantidad INT NOT NULL
+);
+
